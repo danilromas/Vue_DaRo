b607a1b67706f73310480cf7d63354093efb1354
danilromas <furiblack904@gmail.com>
2023-02-05 16:38:08 +0300

    objet_data

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script>
   export default{
     name:'App',
-  }
+	data() {
+		return {
+		name: 'Danil',
+		group: 'I-1-21',
+		}
+	}
+}
 </script>
 
 <template>
 <div> 
-    <h1> Hi, this is my Vue page </h1>
+    <h1> Hi, my name is {{name}} this is my Vue page {{group}} </h1>
     <p> I don't undestand this :(</p>
   </div>
 </template>
