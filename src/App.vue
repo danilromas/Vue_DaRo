95b552205cd148098eed32639fa9c180fc14b80f
danilromas <furiblack904@gmail.com>
2023-02-05 16:30:12 +0300

    vue_3

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script>
+  export default{
+    name:'App',
+  }
+</script>
+
+<template>
+<div> 
+    <h1> Hi, this is my Vue page </h1>
+    <p> I don't undestand this :(</p>
+  </div>
+</template>
