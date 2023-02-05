e23d24fd7cacc1ea505d2e10a062d6a5a7756310
danilromas <furiblack904@gmail.com>
2023-02-05 16:55:00 +0300

    massive

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,7 @@
     image: 'welcome.png',
     text: 'page',
 		href: 'page.html',
-    num1: 1,
-    num2: 3,
-    num3: 5,
+    num: [5,1,3],
 		}
 	}
 }
@@ -22,6 +20,6 @@
     <h1> Hi, my name is {{name}} this is my Vue page {{group}} </h1>
     <p> I don't undestand this :(</p>
     <p><a href={{href}}>{{text}}</a></p>
-    <p>{{ num1 + num2 + num3 }}</p>
+    <p>{{ num[0]+num[1]+num[2]}}</p>
   </div>
 </template>
