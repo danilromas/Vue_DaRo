248b9ed60ac6d1a7f724e41940fda8045b6b37b3
danilromas <furiblack904@gmail.com>
2023-02-27 21:01:04 +0300

    20_0_0

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,9 @@
     amount: 10,
     visible: true,
     hidden: false,
+    visible1: false,
+		visible2: false,
+    visible3: false,
 		}
 	},
   methods: 
@@ -32,6 +35,18 @@
 		this.visible = !this.visible;
         },
     computed: {
+      toggle: function() {
+	    this.visible1 = !this.visible1;
+    },
+        toggle1: function() {
+	    this.visible2 = !this.visible2;
+    },
+        toggle2: function() {
+	    this.visible3 = !this.visible3;
+    },
+    computed: 
+    {
+    }
     }
   }
 }
@@ -44,5 +59,13 @@
     <button @click="show">показать</button>
     <button @click="reversal">скрыть/показать</button>
     <button @click="reversal">скрыть/показать</button>
+    <button @click="toggle1">1</button>
+	<p v-if="visible1">Текст Текст Текст</p>
+<br>
+    <button @click="toggle2">2</button>
+    <p v-if="visible2">Текст Текст Текст</p>
+<br>
+    <button @click="toggle3">3</button>
+    <p v-if="visible3">Текст Текст Текст</p>
 </template>
   
