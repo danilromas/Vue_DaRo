81b133edc72d523f7d4f9faff4a18a1ea90f9341
danilromas <furiblack904@gmail.com>
2023-03-05 13:21:44 +0300

    64_потом

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -83,4 +83,6 @@ import SupportIcon from './icons/IconSupport.vue'
     us by
     <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
   </WelcomeItem>
+
+  
 </template>
